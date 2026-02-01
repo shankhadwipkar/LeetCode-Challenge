0542f981d55a89a857c7b29f6c7f1ee016110549
shankhadwipkar <shankhadwipkar2004@gmail.com>
2026-02-01 08:59:15 +0530

    Day:5 - LeetCode Problem 283, Move Zeros

---
+++ src/Array/LC_283MoveZeros.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Arrays;
+
+public class LC_283MoveZeros {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    static void moveZeroes(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+        while (count < nums.length) {
+            nums[count++] = 0;
+        }
+    }
+}
