4292b7a48c66a7957c7a85c43f7d6ee695e0144d
shankhadwipkar <shankhadwipkar2004@gmail.com>
2026-03-11 11:12:14 +0530

    Day: 12 - LeetCode Problem, 13 - Roman to Integer

---
+++ src/String/LC_13RomanToInteger.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.HashMap;
+
+public class LC_13RomanToInteger {
+    public static void main(String[] args) {
+        String s="VII";
+        System.out.println(romanToInt(s));
+    }
+    static int romanToInt(String s) {
+        HashMap<Character,Integer> hm= new HashMap<Character,Integer>();
+        hm.put('I',1);
+        hm.put('V',5);
+        hm.put('X',10);
+        hm.put('L',50);
+        hm.put('C',100);
+        hm.put('D',500);
+        hm.put('M',1000);
+
+        int result=hm.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--){
+            if(hm.get(s.charAt(i))<hm.get(s.charAt(i+1))){
+                result=result+hm.get(s.charAt(i));
+            }
+            else {
+                result=result+hm.get(s.charAt(i));
+            }
+
+
+        }
+        return result;
+    }
+}
