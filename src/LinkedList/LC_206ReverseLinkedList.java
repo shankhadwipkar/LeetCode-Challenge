8c7f0b48e8d128c099d41286cd1dafbb1b3e3f27
shankhadwipkar <shankhadwipkar2004@gmail.com>
2026-03-13 11:10:17 +0530

    Day: 16 - LeetCode Problem, 206 - Reverse Linked List

---
+++ src/LinkedList/LC_206ReverseLinkedList.java
@@ -0,0 +1,66 @@
+package LinkedList;
+
+public class LC_206ReverseLinkedList {
+    // Definition for singly-linked list
+        static class ListNode {
+            int val;
+            ListNode next;
+
+            ListNode(int val) {
+                this.val = val;
+                this.next = null;
+            }
+        }
+
+        // Reverse Linked List method
+        public static ListNode reverseList(ListNode head) {
+
+            ListNode prev = null;
+            ListNode curr = head;
+
+            while (curr != null) {
+
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            return prev;
+        }
+
+
+        // Print linked list
+        public static void printList(ListNode head) {
+
+            ListNode temp = head;
+
+            while (temp != null) {
+                System.out.print(temp.val + " -> ");
+                temp = temp.next;
+            }
+
+            System.out.println("null");
+        }
+
+
+        // Main method
+        public static void main(String[] args) {
+
+            // Create list: 1 -> 2 -> 3 -> 4 -> 5
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+
+            System.out.println("Original List:");
+            printList(head);
+
+            head = reverseList(head);
+
+            System.out.println("Reversed List:");
+            printList(head);
+        }
+}
+
